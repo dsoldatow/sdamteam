dbd0b33eb3471336c2bd1768694c6c3d44029a78
xqzmelol <ii.sv4k@gmail.com>
2021-08-30 23:52:07 +0700

    sdamteam

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-if __name__ == '__main__':
-    response = requests.post(
-        'http://localhost:8080/vk_callback',
-        json={'type': 'message_new', 'group_id': '1', 'object': {'text': 'blabla', 'from_id': '123'}},
-    )
-    print(response.text)
