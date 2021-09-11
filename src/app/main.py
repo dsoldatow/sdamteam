cc28b27dc95b29fe9ba5ad9a587761de7d43ccda
xqzmelol <ii.sv4k@gmail.com>
2021-09-11 20:12:05 +0700

    deploy

--- src/app/main.py
+++ src/app/main.py
@@ -1,11 +1,6 @@
-import uvicorn
 from fastapi import FastAPI
 
 from app.api import vk_callback
 
 app = FastAPI(debug=True)
 app.include_router(vk_callback.router)
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
